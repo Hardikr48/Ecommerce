822e20621e032566e90426d5a650b871bec7b5ec
Hardikr48 <hardikr19326@gmail.com>
2021-03-09 15:44:25 +0530

    update product

--- product/views.py
+++ product/views.py
@@ -9,6 +9,23 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from helper import getNegativeResponse, getPositiveResponse
     @action(detail=False, methods=["post"])
+    def search(self, request):  
+        try:
+            user = self.request.user
+            if user:
+                try:
+                    product = Product.objects.filter(name__startswith=request.data['name'])                
+                except Exception:
+                    data = getNegativeResponse("product is not valid")
+                    return Response(data)
+                serializer = ProductListSerializer(product , many= True)
+                data = getPositiveResponse("product", serializer.data)
+                return Response(data)
+        except Exception:
+            data = getNegativeResponse("unauthorized user")
+            return Response(data)
+
+    @action(detail=False, methods=["post"])
     def add(self, request):
         print(request.data)
         try:
